a3a20636f4390e948194193b19ab404860cf3571
I'm Luis! <malbertoa_11@hotmail.com>
2020-03-22 13:35:40 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,15 @@ module.exports = {
         path: `./src/data/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-33168424-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
