da8d6c0be33ebad8f1de773d3ec8eda3df68566e
I'm Luis! <malbertoa_11@hotmail.com>
2020-03-22 15:03:41 -0400

    Expand footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => {
               Construido con ❤ por <a href="https://twitter.com/zerquix18">Luis</a>. El código fuente está disponible en <a href="https://github.com/zerquix18/coronavirusrd">Github</a>.
             </p>
             <p>
-              Este sitio no está afiliado a Salud Pública ni a ninguna institución. Tampoco pretende hacer recomendaciones o predecir el futuro. Su objetivo principal es el análisis de los datos, cada uno con su fuente.
+              Este sitio no está afiliado a Salud Pública ni a ninguna institución. Tampoco pretende hacer recomendaciones o predecir el futuro. Su objetivo principal es el análisis de los datos, provenientes directamente de Salud Pública. Para más información sobre el coronavirus, por favor visitar el <a href="http://coronavirusrd.gob.do/">sitio oficial de Salud Pública</a>.
             </p>
           </div>
         </footer>
