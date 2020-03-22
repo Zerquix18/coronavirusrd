38af82a6de752ab1d642497fca2553ccc033ad60
I'm Luis! <malbertoa_11@hotmail.com>
2020-03-22 09:55:19 -0400

    Add totales

--- src/components/province-distribution.js
+++ src/components/province-distribution.js
@@ -18,6 +18,14 @@ const ProvinceDistribution = ({ provinces }) => {
     return bTotalCases - aTotalCases
   })
 
+  let total_cases = 0
+  let total_imported = 0
+  let total_under_investigation = 0
+  let total_local = 0
+  let total_new_cases = 0
+  let total_deaths = 0
+  let total_new_deaths = 0
+
   return (
     <div>
       <h2 className="title">Distribución por provincia</h2>
@@ -37,16 +45,24 @@ const ProvinceDistribution = ({ provinces }) => {
           <th>Provincia</th>
           <th>Casos totales</th>
           <th>Casos importados</th>
-          <th>Casos investigados</th>
+          <th>Casos siendo investigados</th>
           <th>Casos locales</th>
           <th>Casos nuevos</th>
           <th>Total muertes</th>
           <th>Nuevas muertes</th>
         </thead>
         <tbody>
-          { provincesToDisplay.map((province, i) => {
+          { provincesToDisplay.map((province) => {
             const name = province.name
             const lastUpdate = province.cases[province.cases.length - 1]
+
+            total_cases += lastUpdate.total_cases
+            total_imported += lastUpdate.total_imported
+            total_under_investigation += lastUpdate.total_under_investigation
+            total_local += lastUpdate.total_local
+            total_new_cases += lastUpdate.new_cases || 0
+            total_deaths += lastUpdate.total_deaths
+            total_new_deaths += lastUpdate.new_deaths || 0
 
             return (
               <tr>
@@ -64,6 +80,13 @@ const ProvinceDistribution = ({ provinces }) => {
 
           <tr>
             <th>TOTALES</th>
+            <th>{ total_cases }</th>
+            <th>{ total_imported }</th>
+            <th>{ total_under_investigation }</th>
+            <th>{ total_local }</th>
+            <th>{ total_new_cases }</th>
+            <th>{ total_deaths }</th>
+            <th>{ total_new_deaths }</th>
           </tr>
         </tbody>
       </table>
