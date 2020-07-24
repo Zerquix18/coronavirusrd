a06425f1249b855bf4b13b36f226e73d390c3570
I'm Luis! <malbertoa_11@hotmail.com>
2020-07-24 13:20:45 -0400

    Add today's update

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Inicio" />
-      <strong>Última actualización: </strong>23 de Julio, 2:04PM
+      <strong>Última actualización: </strong>24 de Julio, 1:20PM
 
       <hr />
 
